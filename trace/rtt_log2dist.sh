aca09b32dccd279951e9957aaedec390b3f57a38
Yukun (Vincent) Zeng <yukun.zeng.hit@outlook.com>
2018-06-15 13:38:24 -0500

    file format change

--- trace/rtt_log2dist.sh
+++ trace/rtt_log2dist.sh
@@ -4,12 +4,12 @@ do
     path=$var
     dist_path=./dist/$fname.dist
     
-    cat $path | grep icmp_seq | cut -d'=' -f4 | cut -d' ' -f1 > tmp_rtt.txt
-    ./maketable tmp_rtt.txt > $dist_path
+    cat $path | grep icmp_seq | cut -d'=' -f4 | cut -d' ' -f1 > tmp_rtt.log
+    ./maketable tmp_rtt.log > $dist_path
     cp $dist_path /usr/lib64/tc/
     
     echo "${fname}" >> dist_db
-    echo "$(./stats tmp_rtt.txt)" >> dist_db
-    rm -rf tmp_rtt.txt
+    echo "$(./stats tmp_rtt.log)" >> dist_db
+    rm -rf tmp_rtt.log
 done
 
